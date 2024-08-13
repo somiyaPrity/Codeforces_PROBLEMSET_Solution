ccd1353c9980b4118fd72447580c576bff71c580
somiyaPrity <somiyakhanp32@gmail.com>
2024-08-13 15:02:27 +0600

    domino pilling

---
+++ 50A_Domino_piling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _50A_Domino_piling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] first_line = Console.ReadLine().Split(' '); //split by space
+            int M = int.Parse(first_line[0]);
+            int N = int.Parse(first_line[1]);
+            int result = (M * N) / 2;
+            Console.WriteLine(result);
+        }
+    }
+}
